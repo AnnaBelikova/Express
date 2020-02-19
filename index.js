77ae02c313295d46a0648adc44205cf68f330f5d
Anna Belikova <bel4onok_spb@mail.ru>
2020-02-20 00:27:51 +0300

    Express Rest API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +11,10 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
+
 
 app.all('/dishes', (req,res,next)=>{
     res.statusCode = 200;
